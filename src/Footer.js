39f563ee66deb525c6e61e8c40bd4282f8c7ee09
AmyPruthi <amypruthi03@gmail.com>
2024-05-03 02:38:07 +0530

    commit

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import { FaHome } from "react-icons/fa";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { LuChefHat } from "react-icons/lu";
+import { MdOutlineFoodBank } from "react-icons/md";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <nav>
+                    <ul>
+                        <li><a href=""><FaHome style={{fontSize : '34px'}}/></a></li>
+                        <li><a href=""><IoFastFoodOutline style={{fontSize : '34px'}}/></a></li>
+                        <li><a href=""><LuChefHat style={{fontSize : '34px'}}/></a></li>
+                        <li><a href=""><MdOutlineFoodBank style={{fontSize : '34px'}}/></a></li>
+                    </ul>
+                </nav>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer
