8d180c6eeb9535f1177e05c011fe9b6e3a0cc6c7
Anirudh1503 <anirudhgoyal151103@gmail.com>
2024-05-03 14:35:54 +0530

    1

--- frontend/fitfuel/src/Navbar.js
+++ frontend/fitfuel/src/Navbar.js
@@ -1,10 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaCalendarAlt, FaUserCircle } from "react-icons/fa";
 import './style.css';
-import { useState } from 'react';
-import { FaCalendarAlt } from "react-icons/fa";
-import { FaCircleUser } from "react-icons/fa6";
 import Logout from './Logout';
-
 
 function getDate() {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -22,15 +19,16 @@ const Navbar = () => {
   const [currentDate, setCurrentDate] = useState(getDate());
   return (
     <div className='header'>
+      <ul>
         <li><a href="#" className="logo">Fit<span>Fuel</span></a></li>  
-        <li><a href=''><FaCalendarAlt style={{ fontSize: '24px' , alignItems : 'cen' }}/> {currentDate.month} {currentDate.date}, {currentDate.day}</a></li>
-        <ul class="navbar">
-          <li><a href=''>Hi, <FaCircleUser style={{ fontSize: '24px' }}/></a></li>
-          <li><Logout /></li>
-        </ul>
-        
-      </div>
-  )
+        <li><a href=''><FaCalendarAlt style={{ fontSize: '24px', alignItems: 'center' }}/> {currentDate.month} {currentDate.date}, {currentDate.day}</a></li>
+      </ul>
+      <ul className="navbar">
+        <li><a href=''>Hi, <FaUserCircle style={{ fontSize: '24px' }}/></a></li>
+        <li><Logout /></li>
+      </ul>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
